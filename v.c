4cde5d951bac36304a8a9a99a1f49482af2e77c0
peter <peter>
2018-07-14 21:28:27 +0000

    clean up nanboxed value implementation and update comments

--- v.c
+++ v.c
@@ -25,7 +25,7 @@
  * as an mmapped type.
  */
 
-typedef enum{ vI, vF, vS, vY, vD, vQ }VT;
+typedef enum{ vA=0, vQ=1, vY=2, vS=3, vD=4, vI=6, vF=7 }VT;
 
 typedef U8B *S;
 typedef U32 Q;
@@ -58,116 +58,91 @@ U32 slen(S s){R ((U32 *)s)[-1];}
 S snew(U32 z, U8B b[z]){S s=ma(z+1+szof(U32));*((U32 *)s)=z;s+=szof(U32);mc(s,b,z);s[z]='\0';R s;}
 void sfree(S s){free(s-szof(U32));}
 
-typedef union {
-	U64 u64;
-	F64 f;
-	A *a;
-	I32 i;
-	S s;
-	Y y;
-	D d;
-	Q q;
-	struct {
-		uint32_t p; // payload
-		uint32_t tag;
-	} bits;
-} V;
+typedef union {U64 u64;F64 f;A *a;} V;
 
-/*
- * 64-bit platforms
- *
+/**
  * This range of NaN space is represented by 64-bit numbers begining with
  * 13 bits of ones. That is, the first 16 bits are 0xFFF8 or higher.  In
  * practice, no higher value is used for NaNs.  We rely on the fact that no
- * valid double-precision numbers will be "higher" than this (compared as an
+ * valid double-precision numbers will be "higher" than this (compared to an
  * uint64).
  *
  * By adding 7 * 2^48 as a 64-bit integer addition, we shift the first 16 bits
  * in the doubles from the range 0000..FFF8 to the range 0007..FFFF.  Doubles
  * are decoded by reversing this operation, i.e. substracting the same number.
  *
- * The top 16-bits denote the type of the encoded nanbox_t:
+ * The top 16-bits denote the type of the encoded value:
  *
- *     Pointer {  0000:PPPP:PPPP:PPPP
- *             /  0001:xxxx:xxxx:xxxx
- *     Aux.   {           ...
- *             \  0005:xxxx:xxxx:xxxx
- *     Integer {  0006:0000:IIII:IIII
- *              / 0007:****:****:****
- *     Double  {          ...
- *              \ FFFF:****:****:****
+ *     Array    { 0000:xxxx:xxxx:xxxx
+ *     Quot     { 0001:0000:xxxx:xxxx
+ *     Symbol   { 0002:0000:xxxx:xxxx
+ *     String   { 0003:xxxx:xxxx:xxxx
+ *     Dict     { 0004:pppp:xxxx:xxxx
+ * (unassigned) { 0005:xxxx:xxxx:xxxx
+ *     Integer  { 0006:0000:xxxx:xxxx
+ *              / 0007:xxxx:xxxx:xxxx
+ *     Double  {         ...
+ *              \ FFFF:xxxx:xxxx:xxxx
  *
- * 32-bit signed integers are marked with the 16-bit tag 0x0006.
- *
- * The tags 0x0001..0x0005 can be used to store five additional types of
- * 48-bit auxillary data, each storing up to 48 bits of payload.
- *
- * The tag 0x0000 denotes a pointer, or another form of tagged immediate.
- * Boolean, 'null', 'undefined' and 'deleted' are represented by specific,
- * invalid pointer values:
- *
- *     False:     0x06
- *     True:      0x07
- *     Undefined: 0x0a
- *     Null:      0x02
- *     Empty:     0x00
- *     Deleted:   0x05
- *
- * All of these except Empty have bit 0 or bit 1 set.
+ * Integers are stored right before doubles. This means any value greater than
+ * 0x0006000000000000ull is a number.
  */
 
-// This value is 7 * 2^48, used to encode doubles such that the encoded value
-// will begin with a 16-bit pattern within the range 0x0007..0xFFFF.
-#define NANBOX_DOUBLE_ENCODE_OFFSET 0x0007000000000000llu
-// If the 16 first bits are 0x0002, this indicates an integer number.  Any
-// larger value is a double, so we can use >= to check for either integer or
-// double.
-#define NANBOX_MIN_NUMBER           0x0006000000000000llu
-#define NANBOX_HIGH16_TAG           0xffff000000000000llu
+#define VTAG                        0xffff000000000000ull
+#define VTQ                         ((U64)vQ<<48)
+#define VTY                         ((U64)vY<<48)
+#define VTS                         ((U64)vS<<48)
+#define VTD                         ((U64)vD<<48)
+#define VTI                         ((U64)vI<<48)
+// No tag for VTF since its upper 16 bits are 0x0007-0xffff. Instead use an offset
+// which gets added to double values to ensure that the encoded value begins with
+// a 16-bit pattern in the range 0x0007-0xFFFF.
+#define VFOFF                       0x0007000000000000ull
 
-// There are 5 * 2^48 auxillary values can be stored in the 64-bit integer
-// range NANBOX_MIN_AUX..NANBOX_MAX_AUX.
-#define NANBOX_MIN_AUX_TAG          0x00010000
-#define NANBOX_MAX_AUX_TAG          0x0005ffff
-#define NANBOX_MIN_AUX              0x0001000000000000llu
-#define NANBOX_MAX_AUX              0x0005ffffffffffffllu
-
-#define NANBOX_TAG_Q                0x0001000000000000llu
-
-// NANBOX_MASK_POINTER defines the allowed non-zero bits in a pointer.
-#define NANBOX_MASK_POINTER         0x0000fffffffffffcllu
-
-// The 'empty' value is guarranteed to consist of a repeated single byte,
-// so that it should be easy to memset an array of nanboxes to 'empty' using
-// NANBOX_EMPTY_BYTE as the value for every byte.
-#define NANBOX_EMPTY_BYTE           0x0
+// Allowed non-zero bits in a pointer.
+#define VMASKPTR         0x0000fffffffffffcull
 
 P vnp(V v){R v.u64==0ull;}
 V vn(void){V v;v.u64=0ull;R v;}
 
-/* true if val is a double or an int */
-P vnump(V v){R v.u64>=NANBOX_MIN_NUMBER;}
+VT vt(V v){R min((v.u64&VTAG)>>48,7);}
 
-P vip(V v){R (v.u64&NANBOX_HIGH16_TAG)==NANBOX_MIN_NUMBER;}
-V i2v(I32 i){V v;v.u64=NANBOX_MIN_NUMBER|(U32)i;R v;}
+/**
+ * True if v is an integer or double. Can check using >= since all number
+ * values will be greater than or equal to vI<<48. This is why vI is 6, so
+ * that int values are encoded contiguous to doubles.
+ */
+P vnump(V v){R v.u64>=VTI;}
+
+P vip(V v){R (v.u64&VTAG)==VTI;}
+V i2v(I32 i){V v;v.u64=VTI|(U32)i;R v;}
 I32 v2i(V v){assert(vip(v));R (I32)v.u64;}
 
 P vfp(V v){R vnump(v)&&!vip(v);}
-V f2v(F64 f){V v;v.f=f;v.u64+=NANBOX_DOUBLE_ENCODE_OFFSET;R v;}
-F64 v2f(V v){assert(vfp(v));v.u64-=NANBOX_DOUBLE_ENCODE_OFFSET;R v.f;}
+V f2v(F64 f){V v;v.f=f;v.u64+=VFOFF;R v;}
+F64 v2f(V v){assert(vfp(v));v.u64-=VFOFF;R v.f;}
 
-P vap(V v){R !(v.u64 & ~NANBOX_MASK_POINTER) && v.u64!=0ull;}
+P vap(V v){R !(v.u64 & ~VMASKPTR) && v.u64!=0ull;}
 V a2v(A *a){V v;v.a=a;R v;}
 A *v2a(V v){assert(vap(v));R v.a;}
 
-P vqp(V v){R v.u64&NANBOX_TAG_Q;}
-V q2v(Q q){V v;v.u64=NANBOX_TAG_Q|q;R v;}
+P vqp(V v){R (v.u64&VTAG)==VTQ;}
+V q2v(Q q){V v;v.u64=VTQ|q;R v;}
 Q v2q(V v){assert(vqp(v));R (Q)v.u64;}
+
+P vyp(V v){R (v.u64&VTAG)==VTY;}
+V y2v(Y y){V v;v.u64=VTY|y;R v;}
+Y v2y(V v){assert(vyp(v));R (Y)v.u64;}
+
+P vsp(V v){R (v.u64&VTAG)==VTS;}
+V s2v(S s){V v;v.u64=VTS|(UIP)s;R v;}
+S v2s(V v){assert(vsp(v));R (S)(v.u64&VMASKPTR);}
 
 void pv(V v)																				{
 	if(vip(v)){printf("%"PRIi32,v2i(v));}
 	else if(vfp(v)){printf("%f",v2f(v));}
 	else if(vqp(v)){printf("$%"PRIu32,v2q(v));}
+	else if(vyp(v)){printf(".%"PRIu32,v2y(v));}
 	else if(vap(v))																			{
 		A *a=v2a(v);
 		for(AZ i=0,rl=a->s[a->r-1];i<a->l;++i)												{
@@ -183,22 +158,37 @@ void pv(V v)																				{
 #if 0
 int main(int argc,char**argv){
 	V v=i2v(42);
-	if(vnump(v)){puts("number");}else{puts("err: should be a number");}
-	if(vip(v)){printf("int %"PRIi32"\n",v2i(v));}else{puts("err: unknown type");}
-	if(vap(v)){puts("err: both i and a");}if(vfp(v)){puts("err: both i and f");}
+	if(vt(v)!=vI){puts("ERR: wrong tag");}
+	if(vnump(v)){puts("number");}else{puts("ERR: should be a number");}
+	if(vip(v)){printf("int %"PRIi32"\n",v2i(v));}else{puts("ERR: unknown type");}
+	if(vap(v)){puts("ERR: both i and a");}if(vfp(v)){puts("ERR: both i and f");}
 	v=f2v(1.23);
-	if(vnump(v)){puts("number");}else{puts("err: should be a number");}
-	if(vfp(v)){printf("double %f\n",v2f(v));}else{puts("err: unknown type");}
-	if(vap(v)){puts("err: both f and a");}if(vip(v)){puts("err: both f and i");}
+	if(vt(v)!=vF){puts("ERR: wrong tag");}
+	if(vnump(v)){puts("number");}else{puts("ERR: should be a number");}
+	if(vfp(v)){printf("double %f\n",v2f(v));}else{puts("ERR: unknown type");}
+	if(vap(v)){puts("ERR: both f and a");}if(vip(v)){puts("ERR: both f and i");}
 	v=q2v(56);
-	if(vnump(v)){puts("err: should not be a number");}else{puts("not a number");}
-	if(vqp(v)){printf("quot: $%"PRIu32"\n",v2q(v));}else{puts("err: unknown type");}
-	if(vap(v)){puts("err: both q and a");}if(vip(v)){puts("err: both q and i");}
+	if(vt(v)!=vQ){puts("ERR: wrong tag");}
+	if(vnump(v)){puts("ERR: should not be a number");}else{puts("not a number");}
+	if(vqp(v)){printf("quot: $%"PRIu32"\n",v2q(v));}else{puts("ERR: unknown type");}
+	if(vap(v)){puts("ERR: both q and a");}if(vyp(v)){puts("ERR: both q and y");}
+	v=y2v(1);
+	if(vt(v)!=vY){puts("ERR: wrong tag");}
+	if(vnump(v)){puts("ERR: should not be a number");}else{puts("not a number");}
+	if(vyp(v)){printf("sym: .%"PRIu32"\n",v2y(v));}else{puts("ERR: unknown type");}
+	if(vqp(v)){puts("ERR: both y and q");}if(vip(v)){puts("ERR: both y and i");}
+	S s=snew(11,(U8B *)"hello world");
+	v=s2v(s);
+	if(vt(v)!=vS){puts("ERR: wrong tag");}
+	if(vnump(v)){puts("ERR: should not be a number");}else{puts("not a number");}
+	if(vsp(v)){printf("str: %.*s\n",slen(v2s(v)),v2s(v));}else{puts("ERR: unknown type");}
+	if(vfp(v)){puts("ERR: both s and f");}if(vap(v)){puts("ERR: both s and a");}
 	A *a=anew(vI,3,(AZ[]){3,3,3});
 	DO(a->l,ai(a)[i]=i+1)
 	v=a2v(a);
-	if(vap(v)){puts("array");printf("rank: %"PRIu8", length: %zu\n", a->r,a->l);pv(v);putchar('\n');}else{puts("err: unknown type");}
-	if(vip(v)){puts("err: both a and i");}if(vfp(v)){puts("err: both a and f");}
+	if(vt(v)!=vA){puts("ERR: wrong tag");}
+	if(vap(v)){puts("array");printf("rank: %"PRIu8", length: %zu\n", a->r,a->l);pv(v);putchar('\n');}else{puts("ERR: unknown type");}
+	if(vip(v)){puts("ERR: both a and i");}if(vfp(v)){puts("ERR: both a and f");}
 	R 0;
 }
 #endif
